b83748cce41cf2a6386c90cde30d6981b6639411
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-03-13 00:37:41 +0100

    Task Completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 let x = "John";
 let y = "Doe";
-console.log(`"${x} <> ${y}"`);
+//console.log(`"${x} <> ${y}"`);
 let myDetails = {
   name: "Sarah",
   surname: "Dennis",
@@ -49,16 +49,29 @@ let findMaxMinNumbers = function () {
   console.log(Math.min(...randomNumberArray));
 };
 //findMaxMinNumbers();
+// Another way
+// function maxMinFunc(numbers) {
+//   let max = numbers[0];
+//   let min = numbers[0];
+//   for (let number of numbers) {
+//     max = number > max ? number : max;
+//     min = number < min ? number : min;
+//   }
+//   return max, min;
+// }
+// console.log(maxMinFunc(randomNumberArray));
 let arrayOfArrays = [];
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 10; i++) {
   let currentArray = [];
-  for (let j = 0; j < 10; j++)
+  for (let j = 0; j < 10; j++) {
     currentArray.push(Math.floor(Math.random() * 10));
+  }
   arrayOfArrays.push(currentArray);
 }
 //console.log(arrayOfArrays);
 
 let largestNumber = function (arr1, arr2) {
+  // return arr1.length > arr2.length ? arr1 : arr2
   if (arr1.length > arr2.length) {
     return arr1;
   } else {
@@ -66,7 +79,18 @@ let largestNumber = function (arr1, arr2) {
   }
 };
 //console.log(largestNumber([1, 3, 5, 6, 7, 8, 8], [0, 1, 2, 4]));
-
+function higherSum(array1, array2) {
+  let firstSum = 0;
+  for (let number of array1) {
+    firstSum += number;
+  }
+  let secondSum = 0;
+  for (let number of array2) {
+    secondSum += number;
+  }
+  return firstSum > secondSum ? firstSum : secondSum;
+}
+//console.log(higherSum([234, 455], [1, 2, 3]));
 // 31) Get the element with an id of "container" from the page
 // 32) Get every <td> element from the page
 // 33) Use a loop for printing the text inside of every <td> element in the page
@@ -79,6 +103,7 @@ let largestNumber = function (arr1, arr2) {
 // 40) Write a function to empty a list
 
 let containerNode = document.getElementById("container");
+
 let tdNode = document.getElementsByTagName("td");
 for (let i = 0; i < tdNode.length; i++) {
   text = tdNode[i].innerText;
@@ -87,5 +112,51 @@ for (let i = 0; i < tdNode.length; i++) {
 
 function headingChange() {
   let headerNode = document.querySelector("#head");
-  headerNode.innerText = "Amazing Fruitiesssssssss";
+  headerNode.innerText = "Amazing Fruits";
 }
+
+function addRowToTable() {
+  let tableBodyNode = document.querySelector("tbody");
+  let newRowNode = document.createElement("tr");
+  for (let i = 0; i < 5; i++) {
+    let td = document.createElement("td");
+    td.innerText = i + 1;
+    newRowNode.appendChild(td);
+  }
+  tableBodyNode.appendChild(newRowNode);
+}
+addRowToTable();
+
+function addTest() {
+  let trNode = document.querySelectorAll("tr");
+  trNode.forEach((tr) => {
+    tr.classList.add("test");
+  });
+}
+addTest();
+function addRedBackground() {
+  let linkNode = document.querySelectorAll("a");
+  linkNode.forEach((a) => {
+    // a.style.backgroundColor = "red"
+    a.classList.add("changeBg");
+  });
+}
+addRedBackground();
+
+window.onload = function pageLoaded() {
+  console.log("Page Loaded");
+};
+// pageLoaded();
+function addItemToList() {
+  let ListNode = document.querySelector("ol");
+  let newItem = document.createElement("li");
+  newItem.innerText = "Sweet";
+  ListNode.appendChild(newItem);
+}
+addItemToList();
+
+function emptyList() {
+  let ListNode = document.querySelector("ul");
+  ListNode.innerHTML = "";
+}
+emptyList();
